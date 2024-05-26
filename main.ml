3cefee86e9c431994909867f42a83c4b2bc20fa1
Laura-Montagnier <128493440+Laura-Montagnier@users.noreply.github.com>
2024-05-26 11:22:10 +0200

    Update main.ml

--- main.ml
+++ main.ml
@@ -3,46 +3,71 @@ open Logosem;;
 open Lexer;;
 open Parser;;
 
-let main () =
-  (* Initialisation de la fenêtre graphique *)
+let run_program in_channel =
+  (* Ouvrir la fenêtre graphique après l'exécution du programme *)
   open_graph " 800x600";
-
-  
-  (* Exemple de séquence d'instructions à évaluer *)
-    let instructions = 
-    Ast.Seq (
-      Avancer (Ast.Int 50),
-      Seq (
-        Gauche (Ast.Int 90),
-        Seq (
-          Avancer (Ast.Int 100),
-          Seq (
-            If (Ast.Bool true, Gauche (Ast.Int 50), Avancer (Ast.Int 200)),
-            For (Ast.Int 3, Avancer (Ast.Int 50))
-          )
-        )
-      )
-    )
-  in
-  
-  (* Création de l'état initial de la tortue *)
-  let state = initial_state in
-  (* Évaluation des instructions *)
-  eval_instruction instructions state;
-  
-  (* Attendre que l'utilisateur ferme la fenêtre *)
-  try
-    while true do
-      if button_down () then
-        let _ = wait_next_event [Button_up] in ()
-      else
-        let _ = wait_next_event [Key_pressed] in ()
-    done
-  with
-  | Graphic_failure _ -> close_graph (); exit 0
+  (* Analyse syntaxique (parsing) *)
+  let lexbuf = Lexing.from_channel in_channel in
+  while true do
+    try
+      Printf.printf "Parsing...\n"; (* Ajouté pour le débogage *)
+      let ast = Parser.main Lexer.read lexbuf in
+      Printf.printf "Parsing réussi.\n"; (* Ajouté pour le débogage *)
+      (* Création de l'état initial de la tortue *)
+      let state = initial_state in
+      (* Évaluation des instructions *)
+      eval_instruction ast state;
+      Printf.printf "Instruction évaluée.\n"; (* Ajouté pour le débogage *)
+      (* Actualiser l'affichage graphique *)
+      synchronize ();
+    with
+    | Lexer.Eof ->
+        Printf.printf "Fin du fichier atteinte.\n";
+        close_graph ();
+        exit 0
+    | Failure msg ->
+        Printf.printf "Erreur de lexer : %s\n" msg;
+        close_graph ();
+        exit 0
+    | Parsing.Parse_error ->
+        let pos = lexbuf.Lexing.lex_curr_p in
+        let line = pos.Lexing.pos_lnum in
+        let col = pos.Lexing.pos_cnum - pos.Lexing.pos_bol in
+        Printf.printf "Erreur de parsing à la ligne %d, colonne %d\n" line col;
+        close_graph ();
+        exit 1
+    | Graphic_failure _ ->
+        Printf.printf "La graphics a foiré.\n";
+        close_graph ();
+        exit 1
+    | e ->
+        Printf.printf "Erreur inconnue : %s\n" (Printexc.to_string e);
+        close_graph ();
+        exit 1
+  done
 ;;
 
+
+let main () =
+  let in_channel =
+    match Array.length Sys.argv with
+    | 1 -> stdin
+    | 2 -> (
+        match Sys.argv.(1) with
+        | "-" -> stdin
+        | name ->
+            (try open_in name with
+            |_ -> Printf.eprintf "Echec ouverture %s\n%!" name ; exit 1)
+       )
+    | n ->
+        Printf.printf "Trop d'arguments sur la ligne de commande.\n" ;
+        exit 1 in
+  run_program in_channel
+;;
+
+
 (* Appel de la fonction main *)
-main ()
+main () ;;
+
 
 
